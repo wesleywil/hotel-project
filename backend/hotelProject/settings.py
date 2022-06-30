2257abfca77cebfcea04a934547da2e13f113e06
wesleywil <wesleywilsonti@gmail.com>
2022-06-30 16:08:42 -0300

    adding .env for private_key use

--- backend/hotelProject/settings.py
+++ backend/hotelProject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+osvcfov84%+u%0+q!sxh1at18ncq0syq3xq$+s19a1#vz#+&q'
+SECRET_KEY = ENV.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
