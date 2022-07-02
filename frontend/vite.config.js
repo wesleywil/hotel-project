a4909573f45ad40edbb0644e39d6230a688ff8c9
wesleywil <wesleywilsonti@gmail.com>
2022-07-02 19:47:22 -0300

    added folder frontend and move all frontend-files to the new folder

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server:{
-    host:'0.0.0.0'
+    host:'0.0.0.0',
+    watch:{
+      ignored:['/backend/']
+    }
   }
 })
